375bb8d9682716efb3b4e96fb4dc4af5c015d57f
lakshmanpilaka <lakshman.pilaka@gmail.com>
2015-07-23 01:05:23 +0530

    commit message

---
+++ app/src/main/java/com/ghr/ubietyapp/sharedPref.java
@@ -0,0 +1,13 @@
+package com.ghr.ubietyapp;
+
+/**
+ * Created by laks on 23/07/2015.
+ */
+public class sharedPref {
+
+    public void sharedPref() {
+//        SharedPreferences.Editor settings = getSharedPreferences(Config.PREFS_NAME, MODE_PRIVATE).edit();
+
+    }
+
+}
